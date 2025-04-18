68ffae8e6db077e48475262132c343ddbfdd7bb8
Mykhailo Ivaskiv <mykhailo@desty.travel>
2025-04-18 14:56:59 +0200

    project init with ts and jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    transform: {
+        "^.+.tsx?$": ["ts-jest", {}],
+    },
+    testMatch: ["**.test.ts"],
+    globals: {
+        "ts-jest": {
+            tsconfig: "tsconfig.json",
+        },
+    },
+};
