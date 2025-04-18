178679d7f01e0bbc65a56a19d2ab5c6c11bd3bb2
Mykhailo Ivaskiv <mykhailo@desty.travel>
2025-04-18 16:29:44 +0200

    test: finish match after 90 seconds passed

--- src/tests/Match.test.ts
+++ src/tests/Match.test.ts
@@ -1,5 +1,7 @@
 import { Match } from "../Match";
 
+jest.useFakeTimers();
+
 test("Creates a match with 0-0 score", () => {
     const match = new Match("Spain", "Brazil");
     expect(match.getSummaryString()).toBe("Spain: 0 - 0 :Brazil");
@@ -37,4 +39,30 @@ test("Creates match, updates score to 1-1 and finishes Match", () => {
 
     expect(match.getSummaryString()).toBe("Spain: 1 - 1 :Brazil");
     expect(match.getTotalAmountOfGoals()).toBe(2);
+});
+
+test("Creates match, updates score to 2-1 and finishes Match after 90 seconds passed", () => {
+    const match = new Match("Spain", "Brazil");
+
+    match.updateScore(1, 0);
+    match.updateScore(1, 1);
+    setTimeout(() => match.updateScore(2, 1), 59_000);
+
+    expect(match.isLive()).toBe(true);
+
+    jest.advanceTimersByTime(45_000);
+
+    expect(match.isLive()).toBe(true);
+    expect(match.getSummaryString()).toBe("Spain: 1 - 1 :Brazil");
+
+    jest.advanceTimersByTime(15_000);
+
+    expect(match.getSummaryString()).toBe("Spain: 2 - 1 :Brazil");
+    expect(match.isLive()).toBe(true);
+
+    jest.advanceTimersByTime(30_000);
+
+    expect(match.isLive()).toBe(false);
+    expect(match.getSummaryString()).toBe("Spain: 2 - 1 :Brazil");
+    expect(match.getTotalAmountOfGoals()).toBe(3);
 });
