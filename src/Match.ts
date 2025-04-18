660abc4ce56c629b0c51acdd3c0aeb65bea5374a
Mykhailo Ivaskiv <mykhailo@desty.travel>
2025-04-18 15:46:10 +0200

    implemented match constructor and summary string

--- src/Match.ts
+++ src/Match.ts
@@ -1,12 +1,17 @@
 export class Match {
     homeTeam: string;
-
     awayTeam: string;
+    homeScore: number;
+    awayScore: number;
 
     constructor(homeTeam: string, awayTeam: string) {
         this.homeTeam = homeTeam;
         this.awayTeam = awayTeam;
+        this.homeScore = 0;
+        this.awayScore = 0;
     }
 
-    getSummaryString() {}
+    getSummaryString() {
+        return `${this.homeTeam}: ${this.homeScore} - ${this.awayScore} :${this.awayTeam}`;
+    }
 }
