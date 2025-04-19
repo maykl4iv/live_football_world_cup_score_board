57532abbecf2ceb5c75fba488ab5687405e60437
Mykhailo Ivaskiv <mykhailo@desty.travel>
2025-04-19 18:32:32 +0200

    test: MatchService.createMatch

---
+++ src/MatchService.ts
@@ -0,0 +1,7 @@
+import { IMatch } from "./types";
+
+export class MatchService {
+    createMatch(homeTeam: string, awayTeam: string): IMatch {
+        return {} as IMatch;
+    }
+}
---
+++ src/tests/MatchService.test.ts
@@ -0,0 +1,18 @@
+import { Match } from "../Match";
+import { MatchService } from "../MatchService";
+
+test("Creates match if doesnt exist", () => {
+    const matchService = new MatchService();
+
+    const match = matchService.createMatch("Spain", "Brazil");
+    expect(match).toBeInstanceOf(Match);
+    expect(match.homeTeam).toBe("Spain");
+    expect(match.awayTeam).toBe("Brazil");
+});
+
+test("Should not create a duplicate match", () => {
+    const matchService = new MatchService();
+
+    const duplicateMatch = matchService.createMatch("Spain", "Brazil");
+    expect(duplicateMatch).toBe(matchService.createMatch("Spain", "Brazil"));
+});
